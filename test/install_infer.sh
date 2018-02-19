4705ab48762c3b006c5f381303680cb15c084018
Brenno Oliveira <brenno@bsd.com.br>
2018-02-19 11:08:05 +0100

    including sudo to some commands to make infer install work

--- test/install_infer.sh
+++ test/install_infer.sh
@@ -3,8 +3,8 @@
 set -ev
 
 # Install Opaml
-wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O /usr/bin/opam
-chmod ugo+x /usr/bin/opam
+sudo wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O /usr/bin/opam
+sudo chmod ugo+x /usr/bin/opam
 opam init --yes --comp=4.01.0 #(then say 'y' to the final question)
 eval `./opam config env`
 opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say 'y' to the question)
@@ -13,6 +13,6 @@ opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say '
 git clone https://github.com/facebook/infer.git
 cd infer/
 ./build-infer.sh
-make install
+sudo make install
 export PATH=`pwd`/infer/bin:$PATH
 cd ../
