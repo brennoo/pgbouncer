a3e9dc565ac1799414702e69b8403c4e16d2814f
Brenno Oliveira <brenno@bsd.com.br>
2018-02-19 11:12:38 +0100

    fixing infer install

--- test/install_infer.sh
+++ test/install_infer.sh
@@ -3,16 +3,16 @@
 set -ev
 
 # Install Opaml
-sudo wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O /usr/bin/opam
-sudo chmod ugo+x /usr/bin/opam
-opam init --yes --comp=4.01.0 #(then say 'y' to the final question)
+wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O opam
+chmod ugo+x ./opam
+./opam init --yes --comp=4.01.0 #(then say 'y' to the final question)
 eval `./opam config env`
-opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say 'y' to the question)
+./opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say 'y' to the question)
 
 # Install infer (latest version)
 git clone https://github.com/facebook/infer.git
 cd infer/
+export PATH=`pwd`:$PATH
 ./build-infer.sh
-sudo make install
 export PATH=`pwd`/infer/bin:$PATH
 cd ../
